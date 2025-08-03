f80567e82c7d4f19195308a707d0ca384a460662
gregor <gregor@gregtech.eu>
2025-08-03 15:02:09 +0200

    add test for invalid last message input

--- handlers/message_test.go
+++ handlers/message_test.go
@@ -17,6 +17,12 @@ func (m *mockDB) CreateLastMessage(lastMessage *models.LastMessage) error {
 	return m.Err
 }
 
+type invalidMessageInput struct {
+	Title    string `json:"titleasdf"`
+	Content  string `json:"contentasdf"`
+	GroupIDs []uint `json:"groupIDsasdf"`
+}
+
 func TestAddLastMessage(t *testing.T) {
 	assert := assert.New(t)
 	t.Run("valid input", func(t *testing.T) {
@@ -48,5 +54,31 @@ func TestAddLastMessage(t *testing.T) {
 		for i, group := range definedField.Groups {
 			assert.Equal(messageInput.GroupIDs[i], group.ID)
 		}
+	})
+	t.Run("invalid input", func(t *testing.T) {
+		c, w := setupGin()
+		userName := "testname"
+		currentUser := &models.User{ID: 1, Name: &userName}
+		c.Set("currentUser", currentUser)
+		mock := newMockDB(nil)
+		mock.IsAuthorized = true
+
+		// TODO: use struct composition instead of this weird duplication
+		messageInput := &invalidMessageInput{
+			Title:    "uwu",
+			Content:  "I hereby declare AAAAAAAAAAAAAA",
+			GroupIDs: []uint{1, 2, 3, 5}}
+		jsonString, _ := sonic.Marshal(messageInput)
+		c.Request = &http.Request{
+			Body: io.NopCloser(bytes.NewBuffer(jsonString)),
+		}
+
+		handler := handlers.AddLastMessage(mock)
+		handler(c)
+
+		//the field in the mock db we're interested in
+		assert.Equal(http.StatusUnprocessableEntity, w.Code, "http status code should not indicate success")
+		assert.Equal([]models.LastMessage(nil), mock.LastMessages, "there should be no last messages created because the input was invalid")
+
 	})
 }
