be98d9f11bc0c5ec61f3de1b48bba2d4855642fd
gregor <gregor@gregtech.eu>
2025-08-05 15:59:06 +0200

    make name optional during registration

--- handlers/user.go
+++ handlers/user.go
@@ -16,7 +16,7 @@ import (
 
 type RegistrationInput struct {
 	Username string  `json:"username" binding:"required"`
-	Name     *string `json:"name"     binding:"required"`
+	Name     *string `json:"name"`
 	Email    string  `json:"email"    binding:"required,email"`
 	Password string  `json:"password" binding:"required"`
 }
